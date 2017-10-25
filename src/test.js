eccd3dfa9174f01fc4b5d1efb42599f828785fb8
Itec Fischertechnik Israel <hadas.adi@gmail.com>
2017-10-25 10:00:31 +0300

    test upd

--- src/test.js
+++ src/test.js
@@ -886,7 +886,7 @@ var IO = {
 			//buttonStates:		[getButtonState('pressed'), getButtonState('released')],
 			//lightBarrierStates:	[getLightBarrierState('opens'), getLightBarrierState('closes')],
 			motors:				['M1', 'M2', 'M3', 'M4'],
-			stopmotors:				['M1', 'M2', 'M3', 'M4',Lang.getMotor('all')],
+			stopmotors:				['M1', 'M2', 'M3', 'M4',Lang.getAll('all')],
 			motorDirections:	[Lang.getMotorDir('forward'), Lang.getMotorDir('backwards')],
 			
 			
